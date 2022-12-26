01f77e20ce3666b7f01f768d9fc22bb6d6ecfea7
DruvithRao <119730051+DruvithRao@users.noreply.github.com>
2022-12-26 11:10:11 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     int r=pow(a,b);     int x=r%c;     printf("%d",x); }

--- Power_of_a_Number.c
+++ Power_of_a_Number.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
 #include<math.h>
-void pn(int a,int b,int c)
-{
-    int p,e;
-    p=pow(a,b);
-    e=p%c;
-    printf("%d",e);
-}
 int main()
 {
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
-    pn(a,b,c);
+    int r=pow(a,b);
+    int x=r%c;
+    printf("%d",x);
 }
