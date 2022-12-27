7bb385a5b941e4cca8a0c79e8e42b6689605e167
DruvithRao <119730051+DruvithRao@users.noreply.github.com>
2022-12-27 11:41:59 +0530

    #include<stdio.h> int main() {     int n,i,j,p;     scanf("%d%d%d",&n,&j,&p);     for(i=1;i<=12;i++)     {         p=n*i;         printf("%d x %d = %d ",n,i,p);     }
    }

--- Multiplication_Table_1.c
+++ Multiplication_Table_1.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,k;
-    scanf("%d",&n);
+    int n,i,j,p;
+    scanf("%d%d%d",&n,&j,&p);
     for(i=1;i<=12;i++)
     {
-        k=n*i;
+        p=n*i;    
         printf("%d x %d = %d
-",n,i,k);
+",n,i,p);
     }
+
+    
+    
 }
