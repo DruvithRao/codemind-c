319f275aa0f024729d78ed2e10c36a2e09482ed1
DruvithRao <119730051+DruvithRao@users.noreply.github.com>
2022-12-28 11:03:49 +0530

    #include<stdio.h> int main() {     int n,i,d;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     float sum=0;     for(i=0;i<n;i++)     {         sum=sum+a[i];     }     float avg;     avg=sum/n;     printf("%.2f",avg); }

--- average_of_the_array.c
+++ average_of_the_array.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n,i;
+    int n,i,d;
     scanf("%d",&n);
     int a[n];
-     float avg,sum=0;
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
+    float sum=0;
     for(i=0;i<n;i++)
     {
         sum=sum+a[i];
     }
-     avg=sum/n;
+    float avg;
+    avg=sum/n;
     printf("%.2f",avg);
 }
